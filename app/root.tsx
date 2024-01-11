c5496f4f53b3b76568ce191baa12fa7023d88c99
ChayutPom <chayutauitakul@gmail.com>
2024-01-11 19:52:07 +0700

    first

---
+++ app/root.tsx
@@ -0,0 +1,137 @@
+import { json, redirect, LoaderFunctionArgs } from "@remix-run/node";
+import {
+  Form,
+  Links,
+  LiveReload,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+  Outlet,
+  Link,
+  useLoaderData,
+  NavLink,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
+import { useEffect } from "react";
+import { getContacts, type ContactMutation, createEmptyContact } from "./data";
+
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+            >
+              <input
+                id="q"
+                defaultValue={q || ""}
+                aria-label="Search contacts"
+                placeholder="Search"
+                className={searching ? "loading" : ""}
+                type="search"
+                name="q"
+              />
+              <div aria-hidden hidden={!searching} id="search-spinner" />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact: ContactMutation) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? <span>★</span> : null}
+                      </Link>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <div
+          id="detail"
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
+        >
+          <Outlet />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ public/build/_shared/remix_hmr-MPKJPOP7.js
@@ -0,0 +1,9 @@
+import {
+  createHotContext
+} from "/build/_shared/chunk-XLV4WZBE.js";
+import "/build/_shared/chunk-UWV35TSL.js";
+import "/build/_shared/chunk-PNG5AS42.js";
+export {
+  createHotContext
+};
+//# sourceMappingURL=/build/_shared/remix_hmr-MPKJPOP7.js.map
